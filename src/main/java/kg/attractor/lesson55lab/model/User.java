66ff7cd4374095b0f803c074d974c42ef568e60a
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-09 18:32:51 +0600

    add model

---
+++ src/main/java/kg/attractor/lesson55lab/model/User.java
@@ -0,0 +1,19 @@
+package kg.attractor.lesson55lab.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String about;
+    private AccountType accountType;
+}
